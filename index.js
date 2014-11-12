e1ed317689c13560c6169f2702419fa75ff45776
Travis Jacobs <trav221@gmail.com>
2014-11-12 20:03:46 -0330

    Add install script/check in main module. Add .gitignore.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-//install apm package 'atom-dependencies'
+//check if apm atom-dependencies is installed. If not, install it.
+module.exports = {
+  function() installAtomPackage{
+    var sh = require('shelljs');
+    if(!sh.which('apm')){
+      sh.echo("Please make sure apm is installed and in your PATH.");
+      sh.exit(1);
+    }
+    sh.exec('apm install package-dependencies');
+  }
+}
 
-//find system the user is using (windows or mac/linux)
-//find apm command location ('which' on unix; what about windows?)
-
-//see if apm atom-dependencies is installed, if not, install it.
-
-//second feature: activate atom-dependencies:update when this package is required in 'dependencies'
+//future: run atom-dependencies:update when this package is required in 'dependencies'
