5bbf732929c32368e81ce27e19def3d24de9f02c
Travis Jacobs <trav221@gmail.com>
2014-11-12 19:52:33 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+//install apm package 'atom-dependencies'
+
+//find system the user is using (windows or mac/linux)
+//find apm command location ('which' on unix; what about windows?)
+
+//see if apm atom-dependencies is installed, if not, install it.
+
+//second feature: activate atom-dependencies:update when this package is required in 'dependencies'
