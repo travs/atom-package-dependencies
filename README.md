89dd03cbabc7c87b1e1eca492598cd1d39a28988
Travis Jacobs <trav221@gmail.com>
2014-11-13 13:12:25 -0330

    Run install() automatically, when this package is installed.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ##atom-package-dependencies
+
+This is a wrapper/install package that will install [package-dependencies](https://atom.io/packages/package-dependencies) when required in a `package.json` file.
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 //check if apm atom-dependencies is installed. If not, install it.
+
 module.exports = {
-  function() installAtomPackage{
-    var sh = require('shelljs');
-    if(!sh.which('apm')){
-      sh.echo("Please make sure apm is installed and in your PATH.");
-      sh.exit(1);
-    }
-    sh.exec('apm install package-dependencies');
-  }
+  install: install
 }
 
-//future: run atom-dependencies:update when this package is required in 'dependencies'
+var install = function() {
+  var sh = require('shelljs');
+  if(sh.exec('apm ls -b | grep "^package-dependencies@"') === 0) {
+    //abort if atom package-dependencies is already installed
+    sh.echo('apm package "package-dependencies" is already installed.')
+    return;
+  }
+  if(!sh.which('apm')){
+    sh.echo('Please make sure apm is installed and in your PATH.');
+    sh.exit(1);
+  }
+  sh.exec('apm install package-dependencies');
+}
+
+
+
+install();
+
+//future: run atom-dependencies:update when this package is
+//included in 'dependencies' of package.json in any developer's atom package
