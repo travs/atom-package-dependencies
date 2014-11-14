26aa008514e9752bbd4cd936af56933e86b841a2
Travis Jacobs <trav221@gmail.com>
2014-11-14 18:50:16 -0330

    Modularizes code further. Uses shelljs properly.

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 //check if apm atom-dependencies is installed. If not, install it.
+var sh = require('shelljs');
 
 module.exports = {
   install: function() {
-    var sh = require('shelljs');
-    if(sh.exec('apm ls -b | grep "^package-dependencies@"') === 0) {
-      //abort if atom package-dependencies is already installed
-      sh.echo('apm package "package-dependencies" is already installed.')
-      return;
+    checkApmInstalled();
+    if(!pdInstalled()){
+      installPd();
     }
-    if(!sh.which('apm')){
-      sh.echo('Please make sure apm is installed and in your PATH.');
-      sh.exit(1);
-    }
-    sh.exec('apm install package-dependencies');
   }
+}
+
+var checkApmInstalled = function(){
+  if(!sh.which('apm')){
+    sh.echo('Please make sure apm is installed and in your PATH.');
+    sh.exit(1);
+  }
+}
+
+var pdInstalled = function(){
+  var fn = getTempFilename();
+  sh.exec('apm ls -b', {silent: true}).output.to(fn);
+  return !!sh.grep('^package-dependencies@', fn);
+}
+
+var installPd = function(){
+  sh.echo('package-dependencies not installed. Attempting installation now.');
+  var x = sh.exec('apm install package-dependencies');
+  exit(x);
+}
+
+var getTempFilename = function(){
+  return os.tmpdir() + "apmInstalledPacks";
 }
