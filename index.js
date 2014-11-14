1f8a34b581509d64ed318c1ef8e67270f95c6232
Travis Jacobs <trav221@gmail.com>
2014-11-14 00:52:35 -0330

    Move install function back inside modules.

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 //check if apm atom-dependencies is installed. If not, install it.
 
 module.exports = {
-  install: install
-}
-
-var install = function() {
-  var sh = require('shelljs');
-  if(sh.exec('apm ls -b | grep "^package-dependencies@"') === 0) {
-    //abort if atom package-dependencies is already installed
-    sh.echo('apm package "package-dependencies" is already installed.')
-    return;
+  install: function() {
+    var sh = require('shelljs');
+    if(sh.exec('apm ls -b | grep "^package-dependencies@"') === 0) {
+      //abort if atom package-dependencies is already installed
+      sh.echo('apm package "package-dependencies" is already installed.')
+      return;
+    }
+    if(!sh.which('apm')){
+      sh.echo('Please make sure apm is installed and in your PATH.');
+      sh.exit(1);
+    }
+    sh.exec('apm install package-dependencies');
   }
-  if(!sh.which('apm')){
-    sh.echo('Please make sure apm is installed and in your PATH.');
-    sh.exit(1);
-  }
-  sh.exec('apm install package-dependencies');
 }
