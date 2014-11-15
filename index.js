40dbb93f0bd5ae3d425664e0c15b863f9bf8954b
Travis Jacobs <trav221@gmail.com>
2014-11-15 15:51:00 -0330

    Start deprecating shelljs requirements, replacing with node's 'child_process' code.

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
-//check if apm atom-dependencies is installed. If not, install it.
 var sh = require('shelljs');
 var os = require('os');
 var fs = require('fs');
-var JSON = require('JSON');
-
 
 module.exports = {
   getDeps: function() {
-    console.log(getPackageDependencies());
+    //getPackageDependencies(function(x){console.log(x);});
+    //checkDependencies();
+    isInstalled();
+    ;
   }
 }
 
-function checkApmInstalled(){
+function isApmInstalled(){
+  //TODO: replace with native node 'child_process' code
   if(!sh.which('apm')) {
-    sh.echo('Please make sure apm is installed and in your PATH.');
+    console.log('Please make sure apm is installed and in your PATH.');
     sh.exit(1);
   }
+  else{
+    return true;
+  }
 }
 
-function getInstalledList(){
-  //gives path to list of installed atom packages
-  var fn = getTempFilename();
-  sh.exec('apm ls -b', {silent: true}).output.to(fn);
-  return fn;
+function getApmPath(){
+  return atom.packages.getApmPath();
+}
+
+function checkDependencies(callback){
+  var installedList = getInstalledList(
+    function(code, output){
+      console.log(output);
+    });
 }
 
 function isInstalled(pack){
-  //make sure 'installList' is defined by calling getInstallList()
-  return !!sh.grep('^' + pack + '@', installList);
+  //make sure 'installedList' is defined by calling getInstallList()
+  //return !!sh.grep('^' + pack + '@', file);
+  var exec = require('child_process').exec,
+    child;
+  var apm = getApmPath();
+  child = exec(apm + ' ls -b | grep "^" + pack + "@"',
+    function (error, stdout, stderr) {
+      console.log(!!stdOut);
+  });
 }
 
-function getTempFilename(){
-  return os.tmpdir() + "apmInstalledPacks";
-}
-
-function getPackageJsonPath(){
-  var parentRoot = require('parent-root');
-  if(os.platform() === 'win32'){
-    return parentRoot() + '\\package.json';
-  }
-  else return parentRoot() + '/package.json';
-}
-
-function getPackageDependencies(){
-  var path = getPackageJsonPath();
-  var packages = JSON.parse(fs.readFileSync(path, 'utf8'));
-  return packages['package-dependencies'];
+function getPackageDependencies(callback){
+  //finds package that is dependant on this package, then parses JSON and gets 'package-dependencies'
+  getPackageJsonPath(function(path){
+    packages = JSON.parse(fs.readFileSync(path, 'utf8'));
+    callback(packages['package-dependencies']);
+  });
 }
 
 
@@ -54,5 +59,24 @@ var installPd = function(){
   sh.echo('package-dependencies not installed. Attempting installation now.');
   var x = sh.exec('apm install package-dependencies');
   sh.exit(x);
+}
+
+function getTempFilename(){
+  return os.tmpdir() + 'apmInstalledPacks';
+}
+
+function getPackageJsonPath(callback){
+  //returns path to 'package.json' in the Atom package that this is required by
+  var findPkg = require('witwip');
+  findPkg(module.parent, function(err, pkgPath, pkgData) {
+    var path = pkgPath;
+    callback(path);
+  });
+}
+
+function getInstalledList(callback){
+  //gives path to list of installed atom packages
+  var fn = getTempFilename();
+  var child = sh.exec(getApmPath() + ' ls -b', {silent: true, async: true}, function(code, output){console.log(output);});
 }
 */
